930660e30343d2310dfcb3297886925048ad6aee
eesan <98546454+melonade13@users.noreply.github.com>
2022-04-09 15:15:12 +0000

    added validation functions for all the classes

--- data.py
+++ data.py
@@ -1,24 +1,28 @@
-def TypeError():
-    return 'wrong type for data'
+import datetime
+
+class RangeError(Exception):
+    pass
+
+class InvalidDataError(Exception):
+    pass
     
 class Field:
     def __init__(self, name: str, label: str):
         self.name = name
         self.label = label 
 
-    @classmethod
-    def validate(cls): #what am i supposed to validate and what is this function supposed to return
-        pass
         
 class String(Field):
     def __init__(self, name: str, label: str):
         super().__init__(name, label)
     #validate function same as parent class
 
-    @classmethod #is it supposed to be like this?
-    def validate(cls): 
-        if type(self.name) != str or type(self.label) != str:
-            return TypeError
+    @classmethod
+    def validate(cls, arg: str): 
+        if type(arg) != str:
+            raise TypeError(f'{arg} is not string data type')
+
+        return True
 
 
 class Integer(Field):
@@ -26,46 +30,74 @@ class Integer(Field):
         super().__init__(name, label)
 
     @classmethod
-    def validate(cls):
-        pass
+    def validate(cls, arg: int): 
+        if type(arg) != int:
+            raise TypeError(f'{arg} is not integer data type')
+
+        return True
 
 class Age(Field):
     def __init__(self, name: str, label: str):
         super().__init__(name, label)
 
     @classmethod
-    def validate(cls, value):
-        pass
+    def validate(cls, arg: int): 
+        if type(arg) != int:
+            raise TypeError(f'{arg} is not integer data type')
+
+        if 20 < arg > 0: #check that age is of acceptable range for a JC student 
+            raise RangeError(f'{arg} is out of acceptable range')
+
+        return True 
+            
 
 class Year(Field):
     def __init__(self, name: str, label: str):
         super().__init__(name, label)
         
     @classmethod
-    def validate(cls):
-        pass
+    def validate(cls, arg: int):
+        
+        if type(arg) != int:
+                raise TypeError(f'{arg} is not integer data type')
+    
+        if 3000 > arg < 2000: #limit to the 21th century 
+            raise RangeError(f'{arg} is out of acceptable range')
+    
+        return True
         
 class Date(Field):
     def __init__(self, name: str, label: str):
         super().__init__(name, label)
         
     @classmethod
-    def validate(cls):
-        pass
+    def validate(cls, date_text: str):
+        try:
+            datetime.datetime.strptime(date_text, '%Y%m%d')
+        except TypeError:
+            raise TypeError('wrong data type, should be string')
+        except ValueError:
+            raise ValueError("Incorrect data format, should be YYYYMMDD")
         
 class Role(Field):
     def __init__(self, name: str, label: str):
         super().__init__(name, label)
         
     @classmethod
-    def validate(cls):
-        pass
+    def validate(cls, arg: str): 
+        if type(arg) != str:
+            raise TypeError(f'{arg} is not string data type')
+
+        return True
+        
         
 class Category(Field):
     def __init__(self, name: str, label: str):
         super().__init__(name, label)
         
     @classmethod
-    def validate(cls):
-        pass
+    def validate(cls, arg): #should be either Achievement, Enrichment, Leadership, or Service
+        if arg not in ['Achievement', 'Enrichment', 'Leadership', 'Service']:
+            raise InvalidDataError('category not found')
+            
         
