d7a2b3846a09155495ab0f4f20ac547133847ec8
AIDAN TAY MING FENG <98455897+Aidan-tay@users.noreply.github.com>
2022-04-09 17:42:37 +0000

    Fixed some bugs and changed some stuff :D

--- data.py
+++ data.py
@@ -15,7 +15,6 @@ class Field:
 class String(Field):
     def __init__(self, name: str, label: str):
         super().__init__(name, label)
-    #validate function same as parent class
 
     @classmethod
     def validate(cls, arg: str): 
@@ -45,7 +44,7 @@ class Age(Field):
         if type(arg) != int:
             raise TypeError(f'{arg} is not integer data type')
 
-        if 20 < arg > 0: #check that age is of acceptable range for a JC student 
+        if 20 > arg > 0: #check that age is of acceptable range for a JC student 
             raise RangeError(f'{arg} is out of acceptable range')
 
         return True 
@@ -61,7 +60,7 @@ class Year(Field):
         if type(arg) != int:
                 raise TypeError(f'{arg} is not integer data type')
     
-        if 3000 > arg < 2000: #limit to the 21th century 
+        if 3000 > arg > 2000: #limit to the 21th century 
             raise RangeError(f'{arg} is out of acceptable range')
     
         return True
@@ -73,31 +72,19 @@ class Date(Field):
     @classmethod
     def validate(cls, date_text: str):
         try:
-            datetime.datetime.strptime(date_text, '%Y%m%d')
+            datetime.strptime(date_text, '%Y%m%d')
         except TypeError:
-            raise TypeError('wrong data type, should be string')
+            raise TypeError('Wrong data type, should be string')
         except ValueError:
             raise ValueError("Incorrect data format, should be YYYYMMDD")
-        
-class Role(Field):
-    def __init__(self, name: str, label: str):
-        super().__init__(name, label)
-        
-    @classmethod
-    def validate(cls, arg: str): 
-        if type(arg) != str:
-            raise TypeError(f'{arg} is not string data type')
-
-        return True
-        
         
 class Category(Field):
     def __init__(self, name: str, label: str):
         super().__init__(name, label)
         
     @classmethod
-    def validate(cls, arg): #should be either Achievement, Enrichment, Leadership, or Service
-        if arg not in ['Achievement', 'Enrichment', 'Leadership', 'Service']:
-            raise InvalidDataError('category not found')
+    def validate(cls, arg, conditions):
+        if arg not in conditions:
+            raise InvalidDataError('Role or category not found')
             
         
