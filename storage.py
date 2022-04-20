8205bc3a8a75ff75cbba00263f902c5d582d4b99
amos <98579062+amosyzk@users.noreply.github.com>
2022-04-20 07:25:10 +0000

    1. Updated insert function to handle cases where record already exists 2. Created find_some function 3. Updated find_all function to match new requirements 4. Fixed find_latest_id function

--- storage.py
+++ storage.py
@@ -1,10 +1,11 @@
 import sqlite3
 
-def insert(table_name, record: dict):
+def insert(table_name, record):
     """
     Inserts a new record into the table.
     """
-    conn = sqlite3.connect("project uwu.db")
+    
+    conn = sqlite3.connect("test.db")
     cur = conn.cursor()
 
     query = f"INSERT INTO {table_name} VALUES ("
@@ -14,7 +15,10 @@ def insert(table_name, record: dict):
 
     query = query.rstrip(", ") + ");"
 
-    cur.execute(query, tuple(record.values()))
+    try:
+        cur.execute(query, tuple(record.values()))
+    except sqlite3.IntegrityError:   # record already exists
+        return False
 
     conn.commit()
     conn.close()
@@ -26,27 +30,6 @@ def update(table_name, record: dict, **kwargs):
     Returns None if no matching attributes found. 
     """
     pass
-
-    if find_one(table_name, kwargs) is None:
-        return None
-    
-    conn = sqlite3.connect("test.db")
-    cur = conn.cursor()
-
-    query = f"UPDATE {table_name} SET "
-
-    for key, value in record.items():
-        query += f"{key} = {value}, "
-
-    query = query.rstrip(", ") + " WHERE "
-
-    for key, value in kwargs.items():
-        query += f"{key} = {value} AND "
-
-    query = query.rstrip(" AND ") + ";"
-    cur.execute(query)
-    conn.commit()
-    conn.close()
 
 
 def delete(table_name, **kwargs):
@@ -72,32 +55,46 @@ def find_all(table_name, field=None):
     Returns a list of all the values in 'field' in 'table_name'. 
     If field is None, return a list of dictionaries with the whole table's contents.
     """
-    conn = sqlite3.connect("project uwu.db")
+    conn = sqlite3.connect("test.db")
     cur = conn.cursor()
     
     if field is None:
-        cur.execute(f"SELECT * FROM {table_name}") 
+        cur.execute(f"SELECT * FROM {table_name}")
+        records = cur.fetchall()
+        all_records = []
+        headers = [column[0] for column in cur.description]
+
+        for record in records:
+            all_records.append(dict(zip(headers, record)))
+
+        conn.close()
+        return all_records
+        
     else:
-        cur.execute(f"SELECT {field} FROM {table_name}",)
 
-    records = cur.fetchall()
-    all_records = []
-    headers = [column[0] for column in cur.description]
+        try:
+            cur.execute(f"SELECT {field} FROM {table_name}")
+        except sqlite3.OperationalError:    # field given does not exist
+            return None
+        else:
+            records = cur.fetchall()
+            all_records = []
 
-    for record in records:
-        all_records.append(dict(zip(headers, record)))
-    
-    conn.close()
-    return all_records
+            for record in records:
+                for item in record:
+                    all_records.append(str(item))
+
+            conn.close()
+            return all_records
 
 
 def find_one(table_name, **kwargs):
     """
-    Returns a record in a dictionary with attributes matching **kwargs.
-    Returns None is no such record found. 
+    Returns a record with attributes matching **kwargs as a dictionary.
+    Returns None f no such record is found. 
     """
     # to fix: does not work for name field only
-    conn = sqlite3.connect("project uwu.db")
+    conn = sqlite3.connect("test.db")
     cur = conn.cursor()
 
     query = f"SELECT * FROM {table_name} WHERE "
@@ -119,7 +116,37 @@ def find_one(table_name, **kwargs):
     return output
 
 
-def latest_id(table_name):
+def find_some(table_name, **kwargs):
+    """
+    Return all records with attributes matching **kwargs as a list of dictionaries.
+    Returns None if no such record is found. 
+    """
+    conn = sqlite3.connect("test.db")
+    cur = conn.cursor()
+    output = []
+
+    query = f"SELECT * FROM {table_name} WHERE "
+    for key, value in kwargs.items():
+        query += f"{key} = {value} AND "
+
+    query = query.rstrip(" AND ") + ";"
+
+    try:
+        cur.execute(query)
+    except sqlite3.OperationalError:   # record not found
+        return None
+
+    records = cur.fetchall()
+    headers = [column[0] for column in cur.description]
+
+    for record in records:
+        output.append(dict(zip(headers, record)))
+
+    conn.close()
+    return output
+
+
+def find_latest_id(table_name):
     """
     Returns the largest id in the table. 
     """
