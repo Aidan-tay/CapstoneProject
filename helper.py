2426171c7b571b07db381c42c2ca13cf60cdeb7a
AIDAN TAY MING FENG <98455897+Aidan-tay@users.noreply.github.com>
2022-04-22 10:11:04 +0000

    added view fucntionality

--- helper.py
+++ helper.py
@@ -2,13 +2,22 @@ import storage
 
 def view_student(field_attributes):
     '''
-    Converts class_id to class name and id to a list of subject names
+    Converts class_id to class name and id to a list of subject names, club names and activity names
     '''
     field_attributes["class"] = storage.find_one("Class", id=field_attributes.pop("class_id"))["name"]
 
-    subject_list = storage.find_some("Student-Subject", student_id = field_attributes.pop("id"))
+    _id = field_attributes.pop("id")
+    subject_list = storage.find_some("Student-Subject", student_id = _id)
     subject_list = [storage.find_one("Subject", id=x["subject_id"])["name"] for x in subject_list]
-    field_attributes["subjects"] = subject_list
+    field_attributes["Subjects"] = subject_list
+
+    club_list = storage.find_some("Membership", student_id = _id)
+    club_list = [storage.find_one("Club", id=x["club_id"])["name"] for x in club_list]
+    field_attributes["Clubs"] = club_list
+
+    activity_list = storage.find_some("Participation", student_id = _id)
+    activity_list = [storage.find_one("Activity", id=x["activity_id"])["name"] for x in activity_list]
+    field_attributes["Activities"] = activity_list
     
 def view_class(field_attributes):
     '''
